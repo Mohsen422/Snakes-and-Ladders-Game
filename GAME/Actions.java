63a9203a811da9a46e9b82f8cf95a34cb4d9c417
Mohsen422 <hesam4hesam4@gmail.com>
2025-08-23 16:45:30 -0700

    Update Actions.java

--- GAME/Actions.java
+++ GAME/Actions.java
@@ -4,23 +4,22 @@ import java.util.Map;
 public class Actions {
     static Map<Integer,Integer> teleports = new HashMap<>();
     static{
-    teleports.put(17,6);
+    teleports.put(4,14);
+    teleports.put(17,7);
+    teleports.put(9,31);
+    teleports.put(1,38);
     teleports.put(62,19);
-    teleports.put(54,34);
+    teleports.put(21,42);
+    teleports.put(28,84);
     teleports.put(87,36);
+    teleports.put(54,34);
     teleports.put(64,60);
+    teleports.put(51,67);
+    teleports.put(72,91);
     teleports.put(93,73);
     teleports.put(94,75);
     teleports.put(98,79);
-    teleports.put(31,9);
-    teleports.put(38,1);
-    teleports.put(42,21);
-    teleports.put(28,84);
-    teleports.put(84,28);
-    teleports.put(67,51);
-    teleports.put(91,72);
-    teleports.put(99,80);
-
+    teleports.put(80,99);
     }
     static void teleport(Player p)
     {
