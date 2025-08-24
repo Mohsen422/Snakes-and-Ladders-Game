2eb8c44874980cf76c0b0975a0f8839423630151
Mohsen422 <hesam4hesam4@gmail.com>
2025-08-23 17:12:42 -0700

    Update H_player.java

--- GAME/H_player.java
+++ GAME/H_player.java
@@ -1,16 +1,26 @@
 package GAME;
 
 import java.util.Random;
+
+import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 public class H_player extends Player {
     H_player(String Name , String color)
     {
         this.name = Name ;
-        this.piece = new Circle(30) ;
-        this.piece.setFill(Color.BLUE);
+        this.cr = new Circle(30) ;
+        this.piece = new Group();
+        this.cr.setFill(Color.BLUE);
+        Text text = new Text("YOU");
+        text.setFill(Color.WHITE);
+        text.setX(cr.getCenterX() - 10);
+        text.setY(cr.getCenterY() + 5);
+        this.piece.getChildren().add(cr);
+        this.piece.getChildren().add(text);
     }
     public void act()
     {
