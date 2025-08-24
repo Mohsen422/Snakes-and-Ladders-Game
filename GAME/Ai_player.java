5ee90f8d0553dd62cb4ad97902685da179455035
Mohsen422 <hesam4hesam4@gmail.com>
2025-08-23 17:11:52 -0700

    Update Ai_player.java

--- GAME/Ai_player.java
+++ GAME/Ai_player.java
@@ -1,17 +1,27 @@
 
+
 package GAME;
 import java.util.Random;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-
+import javafx.scene.text.Text;
+import javafx.scene.paint.Color;
+import javafx.scene.Group;
 public class Ai_player extends Player {
     Ai_player(String Name , String color)
     {
         this.name = Name ;
-        this.piece = new Circle(30) ;
-        this.piece.setFill(Color.RED);
+        this.cr = new Circle(30) ;
+        this.piece = new Group();
+        this.cr.setFill(Color.RED);
+        Text text = new Text("AI");
+        text.setFill(Color.WHITE);
+        text.setX(cr.getCenterX() - 10);
+        text.setY(cr.getCenterY() + 5);
+        this.piece.getChildren().add(cr);
+        this.piece.getChildren().add(text);
     }
     public void act()
     {
@@ -23,3 +33,4 @@ public class Ai_player extends Player {
     }
 
 }
+
